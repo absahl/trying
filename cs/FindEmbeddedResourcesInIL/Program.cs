7a92a41b36d55eb2150ef17bb365bae387baa806
Saad Abdul Majid <saad.majid@7vals.com>
2026-01-12 12:31:00 +0500

    cs/FindEmbeddedResourcesInIL: New project

---
+++ cs/FindEmbeddedResourcesInIL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: FindEmbeddedResourcesInPE <assembly-path>");
+            return;
+        }
+
+        var assemblyPath = args[0];
+        Console.WriteLine($"May Allah help me find embedded resources in the assembly [{assemblyPath}]");
+
+        var assembly = Assembly.LoadFrom(assemblyPath);
+        foreach (var name in assembly.GetManifestResourceNames())
+        {
+            Console.WriteLine($"Resource name: {name}");
+        }
+    }
+}
